8510fc9e0d9f37abe1a515f37098223c14a0ccb3
Phil S <philipstubbs13@gmail.com>
2018-04-20 06:25:15 -0500

    set up page components

---
+++ client/src/containers/Home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+const Home = () => (
+    <div>
+        <Container style={{ marginTop: 30 }}>
+            <Row>
+                <Col size="md-12">
+                </Col>
+            </Row>
+            <Row>
+                <Col size="md-12">
+
+                </Col>
+            </Row>
+        </Container>
+    </div>
+);
+
+export default Home;
+
